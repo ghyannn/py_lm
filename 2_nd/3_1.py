32b08f73a7b4fa903c90b9543ce383451401dbe4
ghyannn <ghyanmatlab@gmail.com>
2025-07-15 19:49:04 +0530

    Apply 3_1.py changes after pull

--- 2_nd/3_1.py
+++ 2_nd/3_1.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 a = int(input("Enter first number (a): "))
 b = int(input("Enter second number (b): "))
@@ -11,3 +12,17 @@ a = a - b
 
 print("a =", a)
 print("b =", b)
+=======
+a = int(input("Enter first number (a): "))
+b = int(input("Enter second number (b): "))
+
+
+a = a + b
+b = a - b
+a = a - b
+
+
+
+print("a =", a)
+print("b =", b)
+>>>>>>> b611c7e (Update 3_1.py before pulling)
